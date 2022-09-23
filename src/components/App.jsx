4b89ad3793a04cefcb221efcd3cf0234205004bf
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-09-23 20:03:49 +0300

    setstate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  plusGoodPoint = () => {
+    // this.setState;
+  };
+  plusNeutralPoint = () => {
+    console.log('hello');
+  };
+  plusBadPoint = () => {
+    console.log('bad');
+  };
+  render() {
+    return (
+      <div>
+        <section>
+          <h2>Please leave feedback</h2>
+          <div>
+            <button onClick={this.plusGoodPoint}>Good</button>
+            <button onClick={this.plusNeutralPoint}>Neutral</button>
+            <button onClick={this.plusBadPoint}>Bad</button>
+          </div>
+        </section>
+        <section>
+          <h2>Statistics</h2>
+          <div>
+            <p>
+              Good: <span>{this.state.good}</span>
+            </p>
+            <p>
+              Neutral: <span>{this.state.neutral}</span>
+            </p>
+            <p>
+              Bad: <span>{this.state.bad}</span>
+            </p>
+            <p>Total </p>
+            <p>Positive feedback: </p>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,7 @@
+// export const Statistics = (props) = {
+//    return <div>
+//             <button>Good</button>
+//             <button>Neutral</button>
+//             <button>Bad</button>
+//           </div>;
+// };
